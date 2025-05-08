68dd9b8c626d486776ee27070943eddd221d8743
Jatin Bhardwaj <bhardwajjatin093@gmail.com>
2025-05-08 20:54:42 +0530

    n

---
+++ app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import json
+from typing import Optional
+
+app = FastAPI()
+
+
+with open("q-vercel-python.json", "r") as file:
+    students_data = json.load(file)
+
+@app.get("/")
+def read_root(x: Optional[str] = None, y: Optional[str] = None):
+    xMarks = next((student["marks"] for student in students_data if student["name"] == x), None)
+    yMarks = next((student["marks"] for student in students_data if student["name"] == y), None)
+    
+    return {
+        "marks": [
+            xMarks, yMarks
+        ]
+    }
