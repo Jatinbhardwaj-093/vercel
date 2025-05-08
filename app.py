04253c76c8180ac3dd5262f99b01e449d4384c21
Jatin Bhardwaj <bhardwajjatin093@gmail.com>
2025-05-08 21:19:03 +0530

    new route

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ with open("q-vercel-python.json", "r") as file:
     students_data = json.load(file)
 
 @app.get("/")
+def read_root():
+    return {
+        "message": "Hello World"
+    }
+
+@app.get("/api")
 def read_root(x: Optional[str] = None, y: Optional[str] = None):
     xMarks = next((student["marks"] for student in students_data if student["name"] == x), None)
     yMarks = next((student["marks"] for student in students_data if student["name"] == y), None)
