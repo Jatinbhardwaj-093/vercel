bdcec780fa495176525a4e9ecc757901f6ca357e
Jatin Bhardwaj <bhardwajjatin093@gmail.com>
2025-05-10 10:39:54 +0530

    make home copy as api as well

--- app.py
+++ app.py
@@ -17,13 +17,19 @@ app.add_middleware(
 with open("q-vercel-python.json", "r") as file:
     students_data = json.load(file)
 
-@app.get("/")
-def read_root():
-    return {
-        "message": "Hello World"
-    }
-
 @app.get("/api")
+def read_api(x: Optional[str] = None, y: Optional[str] = None):
+    # Default to 0 if parameter is not provided or not found
+    xMarks = next((student["marks"] for student in students_data if student["name"] == x), 0)
+    yMarks = next((student["marks"] for student in students_data if student["name"] == y), 0)
+    
+    return {
+        "marks": [
+            xMarks, yMarks
+        ]
+    }
+    
+@app.get("/")
 def read_api(x: Optional[str] = None, y: Optional[str] = None):
     # Default to 0 if parameter is not provided or not found
     xMarks = next((student["marks"] for student in students_data if student["name"] == x), 0)
